25aab9c35122d277637b80f270cc0e26c03f3333
WALID DRAÂ <waliddraa2@gmail.com>
2023-03-11 13:36:27 +0100

    Add files via upload

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,10 +1,11 @@
 import Link from "next/link";
+import Image from "next/image";
 
 const Navbar = () => {
   return (
     <nav>
       <div className="logo">
-        <h1>NextList</h1>
+        <Image src="/download.png" width={128} height={77} />
       </div>
       <Link href="/">
         <a>HOME</a>
