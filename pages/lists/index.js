3a1aac6a4f94eba695b9f7f12dd73f4acc5aec44
WALID DRAÂ <waliddraa2@gmail.com>
2023-03-11 15:51:30 +0100

    Add files via upload

--- pages/lists/index.js
+++ pages/lists/index.js
@@ -1,7 +1,25 @@
-const Persons = () => {
+import styles from "../../styles/Lists.module.css";
+
+export const getStaticProps = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
+
+  return {
+    props: { persons: data },
+  };
+};
+
+const Persons = ({ persons }) => {
   return (
     <div>
       <h1>ALL PERSONS</h1>
+      {persons.map((person) => (
+        <div key={person.id}>
+          <a className={styles.single}>
+            <h3>{person.name}</h3>
+          </a>
+        </div>
+      ))}
     </div>
   );
 };
